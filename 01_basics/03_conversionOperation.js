602869f343c1a84c56de5fe7371417690c025b62
AkshaySaode <akshaysaode8589@gmail.com>
2025-07-25 11:59:59 +0000

    comparisions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33asa"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" = 33
@@ -19,8 +19,28 @@ let isLoggedIn = 1
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
-// 
+// **************Operations****************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1  ="hello Akshay"
+let str2 = " Saode"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
 
