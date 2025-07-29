703397cca5486962202fded8acaae0f1246f3f80
AkshaySaode <akshaysaode8589@gmail.com>
2025-07-29 05:54:54 +0000

    datesJS

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,13 @@
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatdDate = new Date(2025, 0, 22)
+console.log(myCreatdDate.toDateString());
+
+
+
+
